3c95823248121882a518ac850e73ebd378b093da
joshua <joshuaxin@163.com>
2023-06-10 00:28:09 +0800

    第6章 session登录 引入redis，课程少一节怎么使用redis的

--- 从0开发个人博客_慕课/blog-1/src/conf/db.js
+++ 从0开发个人博客_慕课/blog-1/src/conf/db.js
@@ -1,19 +1,27 @@
 const env = process.env.NODE_ENV // 获取环境变量,不需要引用
 // 这里的值是 package.json script 里面定义的
 let MYSQL_CONFIG
+let REDIS_CONFIG
 
 if(env == 'dev') {
+    
     MYSQL_CONFIG = {
         host: 'localhost',
         user: 'root',
         password: '',
         port: '3306',
         database: 'myblog'
+    }
+
+    REDIS_CONFIG = {
+        port: 6379,
+        host: '127.0.0.1'
     }
 }
 
 // 按理说 ， 这里不应该使用这个配置，应该是线上使用的配置
 if (env == 'production') {
+
     MYSQL_CONFIG = {
         host: 'localhost',
         user: 'root',
@@ -21,10 +29,16 @@ if (env == 'production') {
         port: '3306',
         database: 'myblog'
     }
+
+    REDIS_CONFIG = {
+        port: 6379,
+        host: '127.0.0.1'
+    }
 }
 
 module.exports = {
-    MYSQL_CONFIG
+    MYSQL_CONFIG,
+    REDIS_CONFIG
 }
 
 
--- 从0开发个人博客_慕课/blog-1/src/user.js
+++ 从0开发个人博客_慕课/blog-1/src/user.js
@@ -1,5 +1,12 @@
 const { login } = require('./controller/user')
 const { SuccessModel, ErrorModel } = require('./model/resmodel')
+const {get, set} = require('./db/redis')
+
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
 
 
 const handleUserRouter = (req, res) => {
@@ -7,17 +14,40 @@ const handleUserRouter = (req, res) => {
     const url = req.url
     const path = url.split('?')[0]
 
-    if (method == 'POST' && path == '/api/user/login') {
-        let {username, password} = req.body
+    if (method == 'GET' && path == '/api/user/login') {
+
+        let {username, password} = req.query
         const result = login(username, password)
         return result.then((data) => {
-            console.log(data)
+            console.log(`data`, data)
             if (data.username) {
+                // 设置 session
+                req.session.username = data.username
+                req.session.realname = data.realname
+                console.log(`req.session is :`, req.session)
+                // 同步到 redis 
+                // set(req.sessionId, req.session)
+                // console.log(`修改req.session 下的数据`)
+                // req.session["test"] = 'aaaa'
                 return new SuccessModel()
             }
             return new ErrorModel('登录错误')
         })
         
+    }
+
+    if (method == 'GET' && path == '/api/user/login-test') {
+        // 在 路由处理之前 session 就已经拦截处理，把 http 请求携带的 cookie 挂载到 re.session 上面了
+        if (req.session.username) {
+            return Promise.resolve(
+                new SuccessModel({ 
+                    session: req.session 
+                })
+            )
+        }
+        return Promise.reject(
+            new ErrorModel('尚未登录')
+        )
     }
 
 }
