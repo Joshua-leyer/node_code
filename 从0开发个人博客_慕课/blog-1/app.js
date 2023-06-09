ef502bc3dcfd2bf64f334357911c0befb4754a8e
joshua <joshuaxin@163.com>
2023-06-10 00:28:09 +0800

    第6章 session登录 引入redis，教程缺少一节视频，通过找网上别人的代码修改了

--- 从0开发个人博客_慕课/blog-1/app.js
+++ 从0开发个人博客_慕课/blog-1/app.js
@@ -1,4 +1,5 @@
 const querystring = require('querystring')
+const { get, set } = require('./src/db/redis')
 
 const handleBlogRouter = require('./src/blog')
 const handleUserRouter = require('./src/user')
@@ -6,6 +7,16 @@ const handleUserRouter = require('./src/user')
 
 const log = console.log.bind(console)
 
+
+const SESSION_DATA = {
+    "test": 'lan424'
+}
+
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
 
 // 解析接收路由中 post 的 data 数据
 const getPostData = (req) => {
@@ -51,10 +62,68 @@ const serverHandle = (req, res) => {
     console.log(`url is `, url)
     // 解析 query
     req.query = querystring.parse(url.split('?')[1])
-    log(`req.query is`, req.query)
+   
+    // 解析 cookie
+    req.cookie = {}
+    const cookieStr = req.headers.cookie || ''
+    cookieStr.split(';').forEach(item => {
+        if (!item) {
+            return
+        }
+        let arr = item.split('=')
+        let key = arr[0].trim() 
+        let val = arr[1].trim() 
+        req.cookie[key] = val
+    })
+    console.log(`解析到的 cookie :`, req.cookie)
 
-    // 处理 POST
-    getPostData(req).then(postData => {
+    // 解析 session
+    /*
+    let needSetcookie = false
+    let userId = req.cookie.userid
+    if (userId) {
+        if (!SESSION_DATA[userId]) {
+            SESSION_DATA[userId] = {}
+        }
+    } else {
+        console.log(`配置要设置的cookie 和 session `)
+        needSetcookie = true
+        userId = `${Date.now()}_${Math.random()}`
+        // 注意这里挂载到 req上面，思想就是 这个 req 对象会走过整个路由过程。 具有携带信息的功能。
+        // 这里其实就相当于Session 给提前拦截了，如果发现cookie中的 sessionid， session中保存的有，
+        // 那么就把 这个userId 挂载到 req.session 上面。 后续的 路由就可以根据这个 req.session 来判断是否有登陆过或者存储过一些信息。
+        // 这里蛮重要的， 结果教程几句带过。甚至没有重点说
+        SESSION_DATA[userId] = {}
+        console.log(`配置的东西是`, userId, SESSION_DATA)
+    }
+
+    // 把 session[key] 挂载到 req.session上面，到时候修改 req.session ，由于对象浅拷贝引用的原因。这里的Session本地数据也会被修改。
+
+    req.session = SESSION_DATA[userId]
+    */
+
+      // GET redis 替换 session
+    let needSetCookie = false; 
+    let userId = req.cookie.userid;
+    if( !userId ){ 
+        needSetCookie = true;
+        userId = `${ Date.now() }_${ Math.random() }`
+        set(userId, {}) //初始化 session
+    }
+    // 获取 session
+    req.sessionId = userId
+    get(req.sessionId).then( sessionData => {
+        console.log('sessionData', sessionData)
+        if( sessionData == null ) {
+            set(req.sessionId, {})
+            req.session = {}
+        } else {
+            req.session = sessionData
+        }
+
+        return getPostData(req);
+    })
+    .then( postData => {
         // 把 请求发送过来的内容挂载到 req 上面
         req.body = postData
 
@@ -62,6 +131,9 @@ const serverHandle = (req, res) => {
         const blogResult = handleBlogRouter(req, res)
         if (blogResult) {
             blogResult.then(blogData => {
+                    if (needSetcookie) {
+                        res.setHeader('Set-Cookie',`userid=${userId}; path=/; httpOnly; expiress=${getCookieExpires()}`)
+                    }
                     res.end(
                         JSON.stringify(blogData)
                     )
@@ -75,9 +147,21 @@ const serverHandle = (req, res) => {
         const userResult = handleUserRouter(req, res)
         if (userResult) {
             userResult.then(userData => {
+                if (needSetcookie) {
+                    console.log(`需要设置cookie`)
+                    res.setHeader('Set-Cookie',`userid=${userId}; path=/; httpOnly; expiress=${getCookieExpires()}`)
+                }
+                // console.log(`修改session 以后session 数据是`, SESSION_DATA)
                 res.end(
                     JSON.stringify(userData)
                 )
+            }).catch(err => {
+                if (needSetcookie) {
+                    res.setHeader('Set-Cookie',`userid=${userId}; path=/; httpOnly; expiress=${getCookieExpires()}`)
+                }
+                res.end(
+                    JSON.stringify(err)
+                )            
             })
             return 
         }
--- 从0开发个人博客_慕课/blog-1/src/user.js
+++ 从0开发个人博客_慕课/blog-1/src/user.js
@@ -1,23 +1,54 @@
 const { login } = require('./controller/user')
 const { SuccessModel, ErrorModel } = require('./model/resmodel')
+const { set } = require('./db/redis')
+
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
 
 
 const handleUserRouter = (req, res) => {
     const method = req.method
-    const url = req.url
-    const path = url.split('?')[0]
+    // const url = req.url
+    // const path = url.split('?')[0]
+    console.log(`req.path is`, req.path)
+    if (method == 'POST' && req.path == '/api/user/login') {
 
-    if (method == 'POST' && path == '/api/user/login') {
-        let {username, password} = req.body
+        const { username, password } = req.body
+
         const result = login(username, password)
-        return result.then((data) => {
-            console.log(data)
-            if (data.username) {
+        return result.then( val => {
+            console.log(`val`, val)
+            if (val.username) {
+                // 设置 session
+                req.session.username = val.username
+                req.session.realname = val.realname
+                console.log(`req.session is :`, req.session)
+                // 同步到 redis 
+                set(req.sessionId, req.session)
+                // console.log(`修改req.session 下的数据`)
+                // req.session["test"] = 'aaaa'
                 return new SuccessModel()
             }
             return new ErrorModel('登录错误')
         })
         
+    }
+
+    if (method == 'GET' && path == '/api/user/login-test') {
+        // 在 路由处理之前 session 就已经拦截处理，把 http 请求携带的 cookie 挂载到 re.session 上面了
+        if (req.session.username) {
+            return Promise.resolve(
+                new SuccessModel({ 
+                    session: req.session 
+                })
+            )
+        }
+        return Promise.reject(
+            new ErrorModel('尚未登录')
+        )
     }
 
 }
